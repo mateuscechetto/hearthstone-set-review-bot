485e7023f5247429c81390d07550e9dd34d3b34c
Mateus Cechetto <mateuscechetto@gmail.com>
2022-11-21 23:57:43 -0300

    format frontend and simple loaders

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,24 @@ import axios from 'axios';
 import { useState } from 'react';
 
 function App() {
-  const [room, setRoom] = useState('');
+  const [streamerName, setStreamerName] = useState('');
   const [spreadSheetLink, setSpreadSheetLink] = useState('');
   const [showCard, setShowCard] = useState(false);
   const [currentCard, setCurrentCard] = useState('');
   const [isRecording, setIsRecording] = useState(false);
   const [ratings, setRatings] = useState([]);
+  const [showFormat, setShowFormat] = useState(false);
+  const [loadingFormat, setLoadingFormat] = useState(false);
+  const [loadingCreateBot, setLoadingCreateBot] = useState(false);
 
   const sendSheet = () => {
+    setLoadingCreateBot(true);
     axios.post("/api/createArchive", {
       link: spreadSheetLink,
-      streamerName: room
+      streamerName: streamerName
     }).then(() => {
       setShowCard(true);
+      setLoadingCreateBot(false);
     }).catch((e) => {
       //alert(e);
       alert("Could not create sheet. Please make sure you gave permission to edit it");
@@ -24,7 +29,7 @@ function App() {
 
   const recordChat = () => {
     axios.post("/api/record", {
-      streamerName: room,
+      streamerName: streamerName,
       cardName: currentCard
     }).then(() => {
       setIsRecording(true);
@@ -36,15 +41,28 @@ function App() {
 
   const stopRecording = () => {
     axios.post("/api/stop", {
-      streamerName: room,
+      streamerName: streamerName,
     }).then((res) => {
       setIsRecording(false);
       setCurrentCard('');
       const newRating = res.data;
       setRatings(prevArray => [newRating, ...prevArray]);
+      setShowFormat(true);
     }).catch(() => {
 
     });
+  }
+
+  const formatSheet = () => {
+    setLoadingFormat(true);
+    axios.post("/api/format", {
+      link: spreadSheetLink,
+    }).then(() => {
+      setLoadingFormat(false);
+      alert("Formatted with success");
+    }).catch((e) => {
+      alert(e.message);
+    })
   }
 
   return (
@@ -54,7 +72,7 @@ function App() {
       <div>
         <input
           placeholder='Your twitch name'
-          onChange={(event) => setRoom(event.target.value)}
+          onChange={(event) => setStreamerName(event.target.value)}
           disabled={showCard} />
         <input
           placeholder='Spreadsheet code'
@@ -62,6 +80,19 @@ function App() {
           disabled={showCard} />
         <button onClick={sendSheet} disabled={showCard}>Create bot</button>
       </div>
+      {loadingCreateBot && <h3>Creating bot...</h3>}
+      {showFormat &&
+        (
+          <>
+            <div>
+              <h3>Have you finished your review? Format it!</h3>
+              <button onClick={formatSheet}> Format sheet</button>
+              {loadingFormat && <h3>Formatting...</h3>}
+            </div>
+          </>
+        )}
+
+
       {showCard && (
         <>
           <h3> A new sheet should be created at the bottom of your spreadsheet </h3>
