db5ef6a2695090fb48d9178733b69aee5dec670c
mateuscechetto <mateuscechetto@gmail.com>
2023-06-28 19:49:09 -0300

    Changed card-grid-item design
    This makes clear to know which rating is from the user and which is from chat

--- client/hs-set-review/src/app/app.module.ts
+++ client/hs-set-review/src/app/app.module.ts
@@ -1,24 +1,23 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { ButtonModule } from 'primeng/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-
+import { AvatarModule } from 'primeng/avatar';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { DataViewModule } from 'primeng/dataview';
+import { DialogModule } from 'primeng/dialog';
+import { RatingModule } from 'primeng/rating';
+import { TooltipModule } from 'primeng/tooltip';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CardViewComponent } from './components/card-view/card-view.component';
-import { DataViewModule } from 'primeng/dataview';
-import { CommonModule } from '@angular/common';
-import { CardListItemComponent } from './components/card-list-item/card-list-item.component';
 import { CardGridItemComponent } from './components/card-grid-item/card-grid-item.component';
-import { CardModule } from 'primeng/card';
-import { RatingModule } from 'primeng/rating';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { DialogModule } from 'primeng/dialog';
+import { CardListItemComponent } from './components/card-list-item/card-list-item.component';
+import { CardViewComponent } from './components/card-view/card-view.component';
 import { CardViewModalComponent } from './components/card-view-modal/card-view-modal.component';
-import { CardService } from './services/card.service';
-import { TooltipModule } from 'primeng/tooltip';
 import { RecordChatComponent } from './components/record-chat/record-chat.component';
+import { CardService } from './services/card.service';
 
 
 
@@ -33,18 +32,22 @@ import { RecordChatComponent } from './components/record-chat/record-chat.compon
     RecordChatComponent,
   ],
   imports: [
+    // angular imports
     BrowserModule,
     BrowserAnimationsModule,
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    AppRoutingModule,
+    // primeng imports
+    AvatarModule,
     ButtonModule,
-    DataViewModule,
     CardModule,
-    RatingModule,
+    DataViewModule,
     DialogModule,
+    RatingModule,
     TooltipModule,
+
+    AppRoutingModule,
   ],
   providers: [CardService],
   bootstrap: [AppComponent]
