c6f55cd736f6c1a0852325f795ede61dfddde0a6
mateuscechetto <mateuscechetto@gmail.com>
2024-01-10 15:55:21 -0300

    Add skeleton to home

--- client/src/app/home/feature/home/home.page.ts
+++ client/src/app/home/feature/home/home.page.ts
@@ -1,4 +1,11 @@
-import { DecimalPipe, NgClass, NgIf } from '@angular/common';
+import {
+  AsyncPipe,
+  DecimalPipe,
+  NgClass,
+  NgFor,
+  NgIf,
+  NgTemplateOutlet,
+} from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { AvatarModule } from 'primeng/avatar';
@@ -10,6 +17,8 @@ import { combineLatest } from 'rxjs';
 import { HotCards } from '../../../shared/models/hs-card';
 import { User } from '../../../shared/models/user';
 import { HomeService } from '../../data-access/home.service';
+import { SkeletonModule } from 'primeng/skeleton';
+import { CARDS_MOCK, USERS_MOCK } from './home-data.mock';
 
 @Component({
   selector: 'app-home',
@@ -25,32 +34,40 @@ import { HomeService } from '../../data-access/home.service';
     NgClass,
     TooltipModule,
     DecimalPipe,
+    AsyncPipe,
+    SkeletonModule,
+    NgTemplateOutlet,
+    NgFor,
   ],
   standalone: true,
 })
 export class HomePage {
   title = 'Showdown in the Badlands Card Review';
-  bestCards: HotCards[] = [];
-  worstCards: HotCards[] = [];
-  standardDeviationCards: HotCards[] = [];
-  usersWithRating: User[] = [];
+  bestCards: HotCards[] = CARDS_MOCK;
+  worstCards: HotCards[] = CARDS_MOCK;
+  standardDeviationCards: HotCards[] = CARDS_MOCK;
+  usersWithRating: User[] = USERS_MOCK;
+  loadingUsers = this.homeService.loadingUsers;
+  loadingStats = this.homeService.loadingStats;
 
-  constructor(
-    private homeService: HomeService
-  ) {}
+  // skeletonTableItems = [1, 2, 3, 3, 4];
+
+  constructor(private homeService: HomeService) {}
 
   ngOnInit() {
-    combineLatest([
-      this.homeService.getUsers(),
-      this.homeService.getStats(),
-    ]).subscribe(([users, cards]) => {
-      console.log([users, cards]);
+    this.homeService.getUsers().subscribe({
+      next: (users) => {
+        this.usersWithRating = users.sort((a, b) => b.followers - a.followers);
+      },
+    });
 
-      this.usersWithRating = users.sort((a, b) => b.followers - a.followers);
-      const { bestCards, worstCards, standardDeviationCards } = cards;
-      this.bestCards = bestCards;
-      this.worstCards = worstCards;
-      this.standardDeviationCards = standardDeviationCards;
+    this.homeService.getStats().subscribe({
+      next: (cards) => {
+        const { bestCards, worstCards, standardDeviationCards } = cards;
+        this.bestCards = bestCards;
+        this.worstCards = worstCards;
+        this.standardDeviationCards = standardDeviationCards;
+      },
     });
   }
 }
