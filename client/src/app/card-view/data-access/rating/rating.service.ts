01c9137c3884a0a740c5922bd3b6be942c3dfd19
mateuscechetto <mateuscechetto@gmail.com>
2024-01-11 19:45:34 -0300

    Possible fix for 401 token invalid when try to rate a card on production

--- client/src/app/card-view/data-access/rating/rating.service.ts
+++ client/src/app/card-view/data-access/rating/rating.service.ts
@@ -5,39 +5,54 @@ import { environment } from '../../../../environments/environment';
 import { RatedCardAPIReturn } from '../../../shared/models/hs-card';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RatingService {
+  constructor(private http: HttpClient) {}
 
-  constructor(
-    private http: HttpClient
-  ) { }
-
-  public rateCard(cardName: string, rating: number, userToken: string): Observable<RatedCardAPIReturn> {    
-    return this.http.post<any>(`${environment.apiUrl}/api/rateCard`, { cardName, rating }, {
-      withCredentials: true,
-      headers: {
-        'Authorization': `Bearer ${userToken}`
+  public rateCard(
+    cardName: string,
+    rating: number,
+    userToken: string
+  ): Observable<RatedCardAPIReturn> {
+    return this.http.post<any>(
+      `${environment.apiUrl}/api/rateCard`,
+      { cardName, rating },
+      {
+        withCredentials: true,
+        headers: {
+          Authorization: `Bearer ${userToken}`,
+        },
       }
-    });
+    );
   }
 
   public recordChat(cardName: string, userToken: string): Observable<any> {
-    return this.http.post<any>(`${environment.apiUrl}/api/record`, { cardName }, {
-      withCredentials: true,
-      headers: {
-        'Authorization': `Bearer ${userToken}`
+    return this.http.post<any>(
+      `${environment.apiUrl}/api/record`,
+      { cardName },
+      {
+        withCredentials: true,
+        headers: {
+          Authorization: `Bearer ${userToken}`,
+        },
       }
-    });
+    );
   }
 
-  public stopRecording(cardName: string, userToken: string): Observable<RatedCardAPIReturn> {
-    return this.http.post<any>(`${environment.apiUrl}/api/stop`, { cardName }, {
-      withCredentials: true,
-      headers: {
-        'Authorization': `Bearer ${userToken}`
+  public stopRecording(
+    cardName: string,
+    userToken: string
+  ): Observable<RatedCardAPIReturn> {
+    return this.http.post<any>(
+      `${environment.apiUrl}/api/stop`,
+      { cardName },
+      {
+        withCredentials: true,
+        headers: {
+          Authorization: `Bearer ${userToken}`,
+        },
       }
-    });
+    );
   }
-
 }
