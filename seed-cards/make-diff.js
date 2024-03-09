6ac801d9b9b238ee81f9381da6abea7f7bec1335
mateuscechetto <mateuscechetto@gmail.com>
2024-03-09 18:22:38 -0300

    Update script that creates a diff between 2 json files

--- seed-cards/make-diff.js
+++ seed-cards/make-diff.js
@@ -1,20 +1,41 @@
+/**
+ * Creates a diff file from 2 jsons from Blizzard API https://hearthstone.blizzard.com/en-us/cards.
+ * Usually, expansion cards are revealed in different times, 
+ * so this script is useful to add only the cards that were not added to the application yet.
+ * Input: -o: Path to the older JSON file.
+ *        -n: Path to the newer JSON file.
+ */
+
+
 const fs = require('fs');
-const path = require('path');
+const path = require('path');const yargs = require('yargs');
+
+const argv = yargs
+    .option('old', {
+        alias: 'o',
+        description: 'Path to the older JSON file',
+        demandOption: true,
+    })
+    .option('new', {
+        alias: 'n',
+        description: 'Path to the new JSON file',
+        demandOption: true,
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
 
 function findUniqueCards(json1, json2) {
     const names1 = new Set(json1.cards.map(card => card.name));
     return json2.cards.filter(card => !names1.has(card.name));
 }
 
-const file1 = 'from-api.json'; 
-const file1Path = path.join(__dirname, file1);
+const file1 = path.resolve(argv.old);
+const file1Data = JSON.parse(fs.readFileSync(file1, 'utf8'));
 
-const file1Data = JSON.parse(fs.readFileSync(file1Path, 'utf8'));
-
-const file2 = 'from-api-updated.json'; 
-const file2Path = path.join(__dirname, file2);
-
-const file2Data = JSON.parse(fs.readFileSync(file2Path, 'utf8'));
+const file2 = path.resolve(argv.new);
+const file2Data = JSON.parse(fs.readFileSync(file2, 'utf8'));
 
 const uniqueCards = findUniqueCards(file1Data, file2Data);
 
